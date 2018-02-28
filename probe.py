a37cc1b5b3636d63081b1cce8547962c3d41eb46
root <root@template-debian-stretch.cesnet.cz>
2018-02-28 17:00:44 +0100

    tuning for proper response detection

--- probe.py
+++ probe.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 
 memcached_stats = "\x00\x01\x00\x00\x00\x01\x00\x00\x73\x74\x61\x74\x73\x0d\x0a"
 ans = sr1(IP(dst=args.target)/UDP(sport=12345, dport=11211)/memcached_stats, timeout=args.timeout, verbose=False)
-if ans and ans.haslayer(UDP):
-	print "response detected from %s" % target
+if ans and ans.haslayer(UDP) and (str(ans.payload).find("STAT version") > -1):
+	print "memcached response detected from %s" % target
 	if args.debug:
 		print ans.summary()
